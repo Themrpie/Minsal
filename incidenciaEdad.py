89263c9a243da40b314bb392258404cfb17aa0eb
Themrpie <73369706+Themrpie@users.noreply.github.com>
2022-06-10 20:58:28 -0400

    Add files via upload

--- incidenciaEdad.py
+++ incidenciaEdad.py
@@ -9,19 +9,35 @@ def incidenciaEdad():
 	viejosNoVacunados = []
 	diferencia = []
 	da = [43]
+	edad = 'Total'
+#Columna 7 es incidencia_casos, 8 incidencia_uci, 9 incidencia_def
+# edades: 71 - 80 años	
 	for i in range(len(df)):		
-		if(df.values[i][1] == 'Total'):
+		if(df.values[i][1] == edad):
 			
 			if df.values[i][2] == 'sin esquema completo':
-				viejosNoVacunados.append(df.values[i][9])
+				viejosNoVacunados.append(df.values[i][7])
 			elif df.values[i][2] == 'con esquema completo':
-				viejosVacunados.append(df.values[i][9])							
+				viejosVacunados.append(df.values[i][7])							
 		#viejos.append()
 
 	diferencia = np.subtract(viejosNoVacunados, viejosVacunados)
-	#print(diferencia)
+	total = np.add(viejosNoVacunados, viejosVacunados)
+	diferenciaRelativa = np.divide(diferencia, total)
+	vacunadosRelativo = np.divide(viejosVacunados, total)
+	noVacundosRelativo = np.divide(viejosNoVacunados, total)
 
-	fig = px.bar(df, y = [diferencia,viejosVacunados, viejosNoVacunados], x = range(1,44), title='Incidencia en fallecidos por cada 100.000 personas según estado de vacunación (Todas las edades)')
+	fig = px.bar(df, y = [diferenciaRelativa,vacunadosRelativo, noVacundosRelativo], 
+		x = range(1,45),
+		color_discrete_map= {'wide_variable_0':'red', 'wide_variable_1': 'blue', 'wide_variable_2': 'green'},
+		category_orders={"Estado de vacunación": ["Oceania", "Europe", "Asia"]},
+		title='Incidencia en fallecidos por cada 100.000 personas según estado de vacunación ('+ edad+')')
+
+	fig.update_layout(
+		legend_title_text='Estado de vacunación',
+		xaxis_title="Semana",
+		yaxis_title="Incidencia",
+		)
 	fig.show()
 
 incidenciaEdad()
